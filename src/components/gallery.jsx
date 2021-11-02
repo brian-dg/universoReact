aed152a0b4e78c6fcfe4faf745e93ce620d58322
brian-dg <brian_rc6@hotmail.com>
2021-11-02 12:13:08 -0300

    agregue image

---
+++ src/components/gallery.jsx
@@ -0,0 +1,31 @@
+import { Image } from "./image";
+const divStyle = {
+  width: '45rem',
+  height: '20rem'
+};
+export const Gallery = (props) => {
+  return (
+    <div id='portfolio' className='text-center'>
+      <div className='container'>
+        <div className='section-title'>
+          <h2>Galeria</h2>
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
+            dapibus leonec.
+          </p>
+        </div>
+        <div className='row'>
+          <div className='portfolio-items'  >
+            {props.data
+              ? props.data.map((d, i) => (
+                <div key={`${d.title}-${i}`} className='col-sm-6 col-md-4 col-lg-4'>
+                  <Image title={d.title} largeImage={d.largeImage} smallImage={d.smallImage} />
+                </div>
+              ))
+              : 'Loading...'}
+          </div>
+        </div>
+      </div>
+    </div>
+  )
+}
